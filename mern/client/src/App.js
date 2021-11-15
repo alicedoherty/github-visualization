96f8c4513e75acb9f3e41bc6f60dcca071408c37
Alice Doherty <66687032+alicedoherty@users.noreply.github.com>
2021-11-15 22:22:05 +0000

    getting react js frontend posting data to mongodb atlas

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+// We use Route in order to define the different routes of our application
+import { Route } from "react-router-dom";
+
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import Edit from "./components/edit";
+import Create from "./components/create";
+import RecordList from "./components/recordList";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Route exact path="/">
+        <RecordList />
+      </Route>
+      <Route path="/edit/:id" component={Edit} />
+      <Route path="/create">
+        <Create />
+      </Route>
     </div>
   );
-}
+};
 
 export default App;
