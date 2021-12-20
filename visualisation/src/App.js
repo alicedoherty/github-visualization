b2df39641061aefa1b8e8b17bbe47f06cd897370
Alice Doherty <66687032+alicedoherty@users.noreply.github.com>
2021-12-20 10:40:30 +0000

    basic github api access

---
+++ visualisation/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import NavBar from "./components/NavBar";
+import User from "./components/User";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <User />
+    </div>
+  );
+}
+
+export default App;
