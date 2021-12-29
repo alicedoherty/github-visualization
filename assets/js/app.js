e976419af1000bd1cd393f47d980f1a802447f0e
Alice Doherty <66687032+alicedoherty@users.noreply.github.com>
2021-12-29 13:57:09 +0000

    basic github api calls

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,75 @@
+// Called from index.html when "Let's Go!" is pressed
 function main() {
-    const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-      ];
-    
-    const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-    };
-    
-    const config = {
-        type: 'line',
-        data: data,
-        options: {}
-    };
-    
-    const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    );
+    if (document.getElementById('username').value != '') {
+        var username = document.getElementById('username').value;
+    } else {
+        alert('Please enter a valid username!');
+    }
+
+    if (document.getElementById('auth').value != '') {
+        var auth = document.getElementById('auth').value;
+    } else {
+        var auth = null;
+    }
+
+    getUserData(username, auth);
 }
+
+async function apiCall(url, auth) {
+    
+    var headers = {
+        "Authorization": `Token ${auth}`
+    }
+
+
+    const response = (auth == null) ? await fetch(url) : await fetch(url, {
+        "method": "GET",
+        "headers": headers
+    });
+
+    let data = await response.json();
+    return data;
+}
+
+async function getUserData(username, auth) {
+    let url = `https://api.github.com/users/${username}`;
+    let userData = await apiCall(url, auth).catch(e => console.error(e));
+
+    document.getElementById("test").innerHTML = userData.name;
+}
+
+
+
+//     const labels = [
+//         username,
+//         auth,
+//         'March',
+//         'April',
+//         'May',
+//         'June',
+//       ];
+    
+//     const data = {
+//     labels: labels,
+//     datasets: [{
+//         label: 'My First dataset',
+//         backgroundColor: 'rgb(255, 99, 132)',
+//         borderColor: 'rgb(255, 99, 132)',
+//         data: [0, 10, 5, 2, 20, 30, 45],
+//     }]
+//     };
+    
+//     const config = {
+//         type: 'line',
+//         data: data,
+//         options: {}
+//     };
+    
+//     const myChart = new Chart(
+//         document.getElementById('myChart'),
+//         config
+//     );
+// }
+
 
 
